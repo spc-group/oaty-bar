70e2a9f96ba573ca96a1bde92b42599a8e31def3
Mark Wolfman <canismarko@gmail.com>
2026-05-11 23:09:05 -0500

    Wrote a body for the dispatcher.

--- src/oaty_bar/_dispatch.py
+++ src/oaty_bar/_dispatch.py
@@ -1,15 +1,40 @@
 import argparse
 import asyncio
+import json
 import logging
+from pathlib import Path
 from typing import Sequence
 
 log = logging.getLogger("oaty-bar")
 
 
-async def run_dispatcher():
-    while True:
-        # Just keep the event loop turning
-        await asyncio.sleep(1)
+async def dispatch_new_runs(websocket, dm_api):
+    async for msg in websocket:
+        msg = json.loads(msg)
+        metadata = msg.get("metadata", {})
+        if "stop" not in metadata:
+            # Run is not finished yet, so ignore it for now
+            continue
+        if metadata["stop"].get("exit_status", "") != "success":
+            continue
+        if "dm_exp" not in metadata["start"]:
+            continue
+        # The data management API can tell us where the experiment should save data
+        experiment = await dm_api.experiment(name=metadata["start"]["dm_exp"])
+        target_folder = Path(experiment.data_path)
+        # The specific workflow to execute depends on the goal of the run
+        workflow = "simple"
+        await dm_api.submit_processing_job(
+            workflow=workflow,
+            run_uid=metadata["start"]["uid"],
+            target_folder=str(target_folder),
+        )
+
+
+async def run_dispatcher(args):
+    websocket = None
+    dm_api = None
+    await dispatch_new_runs(websocket, dm_api)
 
 
 def main(argv: Sequence[str] | None = None):
@@ -18,4 +43,4 @@ def main(argv: Sequence[str] | None = None):
         description="Listen for new Tiled runs, and dispatch data management workflows in response.",
     )
     args = parser.parse_args(argv)
-    asyncio.run(run_dispatcher())
+    asyncio.run(run_dispatcher(args))
---
+++ tests/test_dispatch.py
@@ -0,0 +1,95 @@
+import asyncio
+import datetime as dt
+import json
+import urllib
+
+import pytest
+import pytest_asyncio
+from dmax.data_storage import Experiment
+from websockets.asyncio.client import connect
+from websockets.asyncio.server import serve
+
+from oaty_bar._dispatch import dispatch_new_runs
+
+WS_HOST = "127.0.0.1"
+WS_PORT = 4832
+WS_URL = f"ws://{WS_HOST}:{WS_PORT}"
+
+
+# Borrowed from websockets tests
+async def handler(ws):
+    path = urllib.parse.urlparse(ws.request.path).path
+    if path == "/":
+        # The default path is an eval shell.
+        async for expr in ws:
+            value = eval(expr)
+            await ws.send(str(value))
+    elif path == "/crash":
+        raise RuntimeError
+    elif path == "/no-op":
+        pass
+    elif path == "/delay":
+        delay = float(await ws.recv())
+        await ws.close()
+        await asyncio.sleep(delay)
+    else:
+        raise AssertionError(f"unexpected path: {path}")
+
+
+@pytest_asyncio.fixture()
+async def websocket():
+    async with serve(handler, WS_HOST, WS_PORT) as server:
+        async with connect(WS_URL) as client:
+            yield client, server
+
+
+@pytest.mark.asyncio
+async def test_handle_message(websocket, mocker):
+    client, server = websocket
+    api = mocker.AsyncMock()
+    api.experiment.return_value = Experiment(
+        name="cabana-2026-C3",
+        id="12345",
+        primaryStorage={"name": "", "id": 0},
+        experimentStation={"name": "", "id": 0},
+        experimentType={"name": "", "id": 0},
+        createDate=dt.datetime.now(),
+        updateDate=dt.datetime.now(),
+        startDate=dt.datetime.now(),
+        endDate=dt.datetime.now(),
+        dataDirectory="/tmp",
+    )
+    (connection,) = server.connections
+    dispatched = asyncio.create_task(dispatch_new_runs(websocket=client, dm_api=api))
+    # First check that we don't do anything unless there's a stop document
+    await connection.send(
+        json.dumps(
+            {
+                "type": "container-child-metadata-updated",
+                "key": "ABC123",
+                "specs": [],
+                "metadata": {"start": {}},
+            }
+        )
+    )
+
+    await asyncio.sleep(0.01)
+    assert not api.send.called
+    # Now check that we start a new workflow of some sort
+    await connection.send(
+        json.dumps(
+            {
+                "type": "container-child-metadata-updated",
+                "key": "ABC123",
+                "specs": [],
+                "metadata": {
+                    "start": {"uid": "54321", "dm_exp": "cabana-2026-C3"},
+                    "stop": {"exit_status": "success"},
+                },
+            }
+        )
+    )
+    await asyncio.sleep(0.01)
+    assert not api.submit_processing_job.assert_called_once_with(
+        workflow="simple", run_uid="54321", target_folder="/tmp"
+    )
